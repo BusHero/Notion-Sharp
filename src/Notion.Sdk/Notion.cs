059b4245a197535f45690815aab1730a5c3b3239
Petru Cervac <petru.cervac@gmail.com>
2021-10-07 23:20:26 +0300

    Extracted ExceptionFactory to a separate method

--- src/Notion.Sdk/Notion.cs
+++ src/Notion.Sdk/Notion.cs
@@ -1,6 +1,7 @@
 ﻿using Refit;
 
 using System;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -8,32 +9,35 @@ namespace Notion
 {
     public static class Notion
     {
-        public record ErrorDTO(string message, string code, int status);
 
         public static INotion NewClient(string bearerToken)
         {
             return RestService.For<INotion>("https://api.notion.com/v1/", new RefitSettings
             {
                 AuthorizationHeaderValueGetter = () => Task.FromResult(bearerToken),
-                ExceptionFactory = async httpResponseMessage =>
-                {
-                    if (httpResponseMessage.IsSuccessStatusCode)
-                        return await Task.FromResult(default(Exception));
-
-                    var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
-                    var error = await JsonSerializer.DeserializeAsync<ErrorDTO>(stream, new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true,
-                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                    });
-                    var exception = new NotionException(error.message)
-                    {
-                        Code = error.code,
-                        Status = error.status,
-                    };
-                    return await Task.FromResult(exception);
-                }
+                ExceptionFactory = GetException
             });
+        }
+
+        private record ErrorDTO(string message, string code, int status);
+
+        private static async Task<Exception> GetException(HttpResponseMessage httpResponseMessage)
+        {
+            if (httpResponseMessage.IsSuccessStatusCode)
+                return await Task.FromResult(default(Exception));
+
+            var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
+            var error = await JsonSerializer.DeserializeAsync<ErrorDTO>(stream, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+            var exception = new NotionException(error.message)
+            {
+                Code = error.code,
+                Status = error.status,
+            };
+            return await Task.FromResult(exception);
         }
     }
 }
