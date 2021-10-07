6eeaa796394d050119a4dafb208e69e4b462e7f5
Petru Cervac <petru.cervac@gmail.com>
2021-10-07 19:02:14 +0300

    A number of changes here and there

--- src/Notion.Sdk/Notion.cs
+++ src/Notion.Sdk/Notion.cs
@@ -1,18 +1,38 @@
 ﻿using Refit;
 
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Notion
 {
     public static class Notion
     {
+        public record ErrorDTO(string message, string code, int status);
+
         public static INotion NewClient(string bearerToken)
         {
             return RestService.For<INotion>("https://api.notion.com/v1/", new RefitSettings
             {
                 AuthorizationHeaderValueGetter = () => Task.FromResult(bearerToken),
-                ExceptionFactory = httpResponseMessage => Task.FromResult<Exception>(new NotionException())
+                ExceptionFactory = async httpResponseMessage =>
+                {
+                    if (httpResponseMessage.IsSuccessStatusCode)
+                        return await Task.FromResult(default(Exception));
+
+                    var stream = await httpResponseMessage.Content.ReadAsStreamAsync();
+                    var error = await JsonSerializer.DeserializeAsync<ErrorDTO>(stream, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+                    var exception = new NotionException(error.message)
+                    {
+                        Code = error.code,
+                        Status = error.status,
+                    };
+                    return await Task.FromResult(exception);
+                }
             });
         }
     }
--- src/Notion.Sdk/NotionException.cs
+++ src/Notion.Sdk/NotionException.cs
@@ -4,6 +4,9 @@ namespace Notion
 {
     public class NotionException : Exception
     {
+        public int Status { get; set; }
+        public string Code { get; set; }
 
+        public NotionException(string message): base(message) { }
     }
 }
