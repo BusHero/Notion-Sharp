560c522dfa7b74bb5502b82d3ba89f2e6ccf3c57
Cervac Petru <petru.cervac@gmail.com>
2021-11-28 20:20:23 +0200

    Moved the generic version of the converter to a separate file

---
+++ MarkdownExporter/Converter.Generic.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkdownExporter;
+
+public abstract class Converter<T> : Converter
+{
+    public override Option<List<string>> Convert2(object? value, ConverterSettings? settings) => value switch
+    {
+        T t => Convert(t, settings),
+        _ => Option.None<List<string>>()
+    };
+    public abstract Option<List<string>> Convert(T t, ConverterSettings? settings);
+}
--- MarkdownExporter/Converter.cs
+++ MarkdownExporter/Converter.cs
@@ -25,12 +25,4 @@ public abstract partial class Converter
     }
 }
 
-public abstract class Converter<T> : Converter
-{
-    public override Option<List<string>> Convert2(object? value, ConverterSettings? settings) => value switch
-    {
-        T t => Convert(t, settings),
-        _ => Option.None<List<string>>()
-    };
-    public abstract Option<List<string>> Convert(T t, ConverterSettings? settings);
-}
+
