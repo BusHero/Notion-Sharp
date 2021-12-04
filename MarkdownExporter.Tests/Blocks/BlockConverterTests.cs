44419462dc2ad54e50899f082e9392bfbddb18a9
Cervac Petru <petru.cervac@gmail.com>
2021-12-05 01:55:45 +0200

    Removed unnecesary NumberedListConverterTests

--- MarkdownExporter.Tests/Blocks/BlockConverterTests.cs
+++ MarkdownExporter.Tests/Blocks/BlockConverterTests.cs
@@ -9,38 +9,12 @@ using System.Linq;
 
 namespace MarkdownExporter.Tests;
 
-public class ParagraphConverterTests
+public class BlockConverterTests
 {
-    private ConverterSettings Settings { get; } = new ConverterSettings
-    {
-        Converter =
-            new CompositeConverter(
-            new RichTextConverter(
-                Applicable.Link(Formatters.FormatLink)
-                + Applicable.Bold(Formatters.FormatBold)
-                + Applicable.Italic(Formatters.FormatItalic)
-                + Applicable.Strikethrough(Formatters.FormatStike)
-                + Applicable.Underline(Formatters.FormatUnderline)
-                + Applicable.FormatCode(Formatters.FormatCode)
-                + Applicable.FormatColor(Formatters.FormatColor)), 
-            new ParagraphConverter(Substitute.For<INotion>()))
-
-    };
-
-    [Theory]
-    [MemberData(nameof(Paragraphs))]
-    public void ParseParagraph_Passes(Block.Paragraph block, string expectedText)
-    {
-        var expectedResult = new List<string> { expectedText }.ToOption().Select(list => JsonSerializer.Serialize(list));
-
-        var actualResult = Converter.Convert(block, Settings).Select(list => JsonSerializer.Serialize(list));
-
-        Assert.Equal(expectedResult, actualResult, new OptionComparer<string>(EqualityComparer<string>.Default));
-    }
 
     [Theory]
     [MemberData(nameof(ParentAndChildren))]
-    public void ParseParagraph_WithChildren_Succeds(List<Block> blocks, List<string> expectedResult1, Func<INotion, Converter> converterGenerator)
+    public void ParseBlock_Succeds(List<Block> blocks, List<string> expectedResult1, Func<INotion, Converter> converterGenerator)
     {
         var notion = Substitute.For<INotion>();
         foreach (var (parent, child) in blocks.Zip(blocks.Skip(1)))
@@ -56,7 +30,14 @@ public class ParagraphConverterTests
         {
             Converter = new CompositeConverter(
                 converter,
-                new RichTextConverter(Applicable.ToAplicable((RichText _, string text) => text)))
+                new RichTextConverter(
+                    Applicable.Link(Formatters.FormatLink)
+                    + Applicable.Bold(Formatters.FormatBold)
+                    + Applicable.Italic(Formatters.FormatItalic)
+                    + Applicable.Strikethrough(Formatters.FormatStike)
+                    + Applicable.Underline(Formatters.FormatUnderline)
+                    + Applicable.FormatCode(Formatters.FormatCode)
+                    + Applicable.FormatColor(Formatters.FormatColor)))
         };
         var expectedResult = 
             expectedResult1
@@ -71,11 +52,12 @@ public class ParagraphConverterTests
     }
     
     private static Converter GetParagraphConverter(INotion notion) => new ParagraphConverter(notion);
+    private static Converter GetNumberedListItemConverter(INotion notion) => new NumberedListItemConverter(notion);
 
-    public static TheoryData<Block.Paragraph, string> Paragraphs { get; } = new()
+    public static TheoryData<IEnumerable<Block>, List<string>, Func<INotion, Converter>> ParentAndChildren { get; } = new()
     {
         {
-            new Block.Paragraph
+            Lists.Of<Block>(new Block.Paragraph
             {
                 Text = new RichText[]
                 {
@@ -85,11 +67,12 @@ public class ParagraphConverterTests
                         PlainText = "Some text here and there"
                     }
                 }
-            },
-            "Some text here and there"
+            }),
+            Lists.Of("Some text here and there"),
+            GetParagraphConverter
         },
         {
-            new Block.Paragraph
+            Lists.Of<Block>(new Block.Paragraph
             {
                 Text = new RichText[]
                 {
@@ -104,11 +87,12 @@ public class ParagraphConverterTests
                         PlainText = "here and there"
                     }
                 }
-            },
-            "Some text here and there"
+            }),
+            Lists.Of("Some text here and there"),
+            GetParagraphConverter
         },
         {
-            new Block.Paragraph
+            Lists.Of<Block>(new Block.Paragraph
             {
                 Text = new RichText[]
                 {
@@ -132,12 +116,45 @@ public class ParagraphConverterTests
                         PlainText = "here and there",
                     }
                 }
-            },
-            "*Some text ***here and there**"
+            }),
+            Lists.Of("*Some text ***here and there**"),
+            GetParagraphConverter
         },
-    };
-    public static TheoryData<List<Block>, List<string>, Func<INotion, Converter>> ParentAndChildren { get; } = new()
-    {
+        {
+            Lists.Of<Block>(new Block.NumberedListItem
+            {
+                Text = new[]
+                {
+                    new RichText.Text
+                    {
+                        Content = "Some text here and there",
+                        PlainText = "Some text here and there"
+                    }
+                }
+            }),
+            Lists.Of("1. Some text here and there"),
+            GetNumberedListItemConverter
+        },
+        {
+            Lists.Of<Block>(new Block.NumberedListItem
+            {
+                Text = new[]
+                {
+                    new RichText.Text
+                    {
+                        Content = "Some text",
+                        PlainText = "Some text"
+                    },
+                    new RichText.Text
+                    {
+                        Content = " here and there",
+                        PlainText = " here and there"
+                    }
+                }
+            }),
+            Lists.Of("1. Some text here and there"),
+            GetNumberedListItemConverter
+        },
         {
             new List<Block>
             {
@@ -214,6 +231,81 @@ public class ParagraphConverterTests
             Lists.Of("Parent", "&nbsp;&nbsp;&nbsp;&nbsp;Child", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Grandchild"),
             GetParagraphConverter
         },
+        {
+            Lists.Of<Block>
+            (
+                new Block.NumberedListItem
+                {
+                    Id = Guid.NewGuid(),
+                    HasChildren = true,
+                    Text = new RichText[]
+                    {
+                        new RichText.Text
+                        {
+                            Content = "Parent",
+                            PlainText = "Parent"
+                        }
+                    }
+                },
+                new Block.NumberedListItem
+                {
+                    HasChildren = false,
+                    Text = new RichText[]
+                    {
+                        new RichText.Text
+                        {
+                            Content = "Child",
+                            PlainText = "Child"
+                        }
+                    },
+                }
+            ),
+            Lists.Of("1. Parent", "&nbsp;&nbsp;&nbsp;&nbsp;1. Child"),
+            GetNumberedListItemConverter
+        },
+        {
+            Lists.Of<Block>(
+                new Block.NumberedListItem
+                {
+                    Id = Guid.NewGuid(),
+                    HasChildren = true,
+                    Text = new RichText[]
+                    {
+                        new RichText.Text
+                        {
+                            Content = "Parent",
+                            PlainText = "Parent"
+                        }
+                    }
+                },
+                new Block.NumberedListItem
+                {
+                    Id = Guid.NewGuid(),
+                    HasChildren = true,
+                    Text = new RichText[]
+                    {
+                        new RichText.Text
+                        {
+                            Content = "Child",
+                            PlainText = "Child"
+                        }
+                    },
+                },
+                new Block.NumberedListItem
+                {
+                    Text = new RichText[]
+                    {
+                        new RichText.Text
+                        {
+                            Content = "Grandchild",
+                            PlainText = "Grandchild"
+                        }
+                    }
+                }
+            ),
+            Lists.Of("1. Parent", "&nbsp;&nbsp;&nbsp;&nbsp;1. Child", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. Grandchild"),
+            GetNumberedListItemConverter
+        }
     };
 }
 
