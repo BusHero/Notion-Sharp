758a837fd9cdb43c0361c63283cdf716b0e11944
Cervac Petru <petru.cervac@gmail.com>
2023-03-24 23:07:02 +0200

    chore: add test deleted page (#65)

--- tests/Notion.Sharp.Tests/RetrievePageTests.cs
+++ tests/Notion.Sharp.Tests/RetrievePageTests.cs
@@ -15,14 +15,28 @@ public class RetrievePageTests: NotionTestsBase
         //assert
         using (new AssertionScope())
         {
-            page.Should().NotBeNull();
             page.Id.Should().Be(ValidPageId);
             page.Archived.Should().Be(false);
             page.Properties.Should().ContainKey("title");
             page.Cover.Should().BeNull();
             page.Icon.Should().BeNull();
             (page.Parent as Parent.Page)?.Id.Should().Be(ParentPage);
-            
+        }
+    }
+
+    [Fact]
+    public async Task RemovedPageShouldBeShownAsArchived()
+    {
+        //arrange
+        
+        //act 
+        var page = await SUT.GetPageAsync(DeletedPage);
+        
+        //assert
+        using (new AssertionScope())
+        {
+            page.Id.Should().Be(DeletedPage);
+            page.Archived.Should().Be(true);
         }
     }
 }
