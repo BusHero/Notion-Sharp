948a56296b54c005e22148550e4509bdd2d5a1d8
bus1hero <petru.cervac@gmail.com>
2023-03-26 12:08:37 +0300

    chore: add tests for covers

--- tests/Notion.Sharp.Tests/PageTests.cs
+++ tests/Notion.Sharp.Tests/PageTests.cs
@@ -107,4 +107,73 @@ public class PageTests: NotionTestsBase
             externalFile.ExpireTime.Should().BeSameDateAs(DateTime.Now);
         }
     }
+
+    [Fact]
+    public async Task GetPageWithCover()
+    {
+        // act
+        var page = await SUT.GetPageAsync(Pages.PageWithCover.ToGuid());
+        
+        // assert
+        using (new AssertionScope())
+        {
+            var externalFile = page.Cover as File.External;
+            externalFile!.Name.Should().BeNull();
+            externalFile!.Caption.Should().BeNull();
+            externalFile.Uri.Should().Be("https://www.notion.so/images/page-cover/gradients_8.png");
+        }
+    }
+    
+    [Fact]
+    public async Task GetPageWithCustomLinkCover()
+    {
+        // act
+        var page = await SUT.GetPageAsync(Pages.PageWithCustomLinkCover.ToGuid());
+        
+        // assert
+        using (new AssertionScope())
+        {
+            var externalFile = page.Cover as File.External;
+            externalFile!.Name.Should().BeNull();
+            externalFile!.Caption.Should().BeNull();
+            externalFile.Uri.Should()
+                .Be("https://www.google.com/images/branding" +
+                    "/googlelogo/1x/googlelogo_light_color_272x92dp.png");
+        }
+    }
+    
+    [Fact]
+    public async Task GetPageWithUnslpashCover()
+    {
+        // act
+        var page = await SUT.GetPageAsync(Pages.PageWithUnsplashCover.ToGuid());
+        
+        // assert
+        using (new AssertionScope())
+        {
+            var externalFile = page.Cover as File.External;
+            externalFile!.Name.Should().BeNull();
+            externalFile!.Caption.Should().BeNull();
+            externalFile.Uri.Should()
+                .Be("https://images.unsplash.com/photo-1511300636408-a63a89df3482" +
+                    "?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb");
+        }
+    }
+    
+    [Fact]
+    public async Task GetPageWithUploadedCover()
+    {
+        // act
+        var page = await SUT.GetPageAsync(Pages.PageWithUploadedCover.ToGuid());
+        
+        // assert
+        using (new AssertionScope())
+        {
+            var externalFile = page.Cover as File.Internal;
+            externalFile!.Name.Should().BeNull();
+            externalFile!.Caption.Should().BeNull();
+            externalFile.Uri.Should().NotBeNull();
+            externalFile.ExpireTime.Should().BeSameDateAs(DateTime.Now);
+        }
+    }
 }
