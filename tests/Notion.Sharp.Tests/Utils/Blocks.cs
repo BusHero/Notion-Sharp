66d2789f7c7e379db24ec63f0f2bd7f09655567d
Cervac Petru <petru.cervac@gmail.com>
2023-03-27 20:01:07 +0300

    feat: add more block tests (#80)

--- tests/Notion.Sharp.Tests/Utils/Blocks.cs
+++ tests/Notion.Sharp.Tests/Utils/Blocks.cs
@@ -25,8 +25,11 @@ public static class Blocks
     public const string WebBookmark = "5a07250e4ea24085a413a9c16c79abd5";
     public const string WebBookmarkEmpty = "f54f9df5393a4a21989d87fc53c3d4cc";
     public const string Video = "7cd9de14a9444617beaa3f1bd39d0898";
+    public const string VideoUploaded = "9b6758d5175442368e82478ae1d63f77";
     public const string VideoEmpty = "5fca98d552814e9199fd75bace88192d";
-    public const string AudioEmpty = "0d96f5dc46904f1986183a9b5abc208d";
+    public const string AudioUploaded = "0d96f5dc46904f1986183a9b5abc208d";
+    public const string Audio = "049eba1465ea456abe8d006ab9edf394";
+    public const string AudioEmpty = "aa91f00733b14d16bbd338ac9e3d94e7";
     public const string Code = "9e13d8a3cf4d4d4ab6da00c510545228";
     public const string CodeWithCaption = "b3b8facd970047e0888f3831fdef80dd";
     public const string CodeCSharp = "81e9566ea851466299d91367f3826470";
@@ -34,4 +37,20 @@ public static class Blocks
     public const string File = "6891a6196b2b435b863347e50a182d49";
     public const string FileEmpty = "65dbf4c394ab48938e3686a8ba3458d0";
     public const string FileWithCaption = "e2e226bf62914eabb39f02c83e457866";
+    public const string TableOfContents = "02181c9d86964851b06bd9d8f6eca0b7";
+    
+    public const string Equation = "a56437ddb9c746948596b536cea44705";
+    public const string EquationEmpty = "33c2de6663af4abb9dd660ebe01e0420";
+    public const string Breadcumb = "1ef90db1d1ad4235bfad09168c1bc7f3";
+    public const string SyncBlockOriginal = "fc568f027c724f78be713bb54bdc0db4";
+    public const string SyncdBlockCopy = "0509247fc10543f2addb5e364baea9f7";
+    public const string Button = "f5c8800f1bbf477682afd6b6e42ce94c";
+    public const string ToggledHeading1 = "b55707fb7c99456ea99c73c309615d05";
+    public const string ToggledHeading2 = "88ea0ff5245f4b92beaa14258755121e";
+    public const string ToggledHeading3 = "66c8421fbe7741868cdf143d61a5a3c5";
+    public const string Column = "ed7c918cec3f440eb34906810f04ec0f";
+    public const string Embed = "c629f160608645108a97877d2608e9f2";
+    public const string TwitterEmbed = "49bb7c26f03d4167985f372a5a5a7d7e";
+    public const string ChildDatabase = "444ae2d9933847faa31a2d04689538be";
+    public const string LinkedDatabase = "8fddf23451524922b113a46f272c7507";
 }
