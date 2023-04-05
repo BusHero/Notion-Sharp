93b78211e01f415e54efb85de5319a6496e6414c
Cervac Petru <petru.cervac@gmail.com>
2023-04-05 22:58:00 +0300

    build: add support for Nuke (#123)

---
+++ build/Build.cs
@@ -0,0 +1,44 @@
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+// ReSharper disable UnusedMember.Local
+
+namespace _build;
+
+class Build : NukeBuild
+{
+	public static int Main() => Execute<Build>(_ => _.Compile);
+
+	[Parameter] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+	[Solution] readonly Solution Solution;
+
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+			DotNetRestore(_ => _
+				.SetProjectFile(Solution));
+		});
+
+	Target Compile => _ => _
+		.DependsOn(Restore)
+		.Triggers(Test)
+		.Executes(() =>
+		{
+			DotNetBuild(_ => _
+				.SetProjectFile(Solution)
+				.EnableNoRestore()
+				.SetConfiguration(Configuration));
+		});
+	
+	Target Test => _ => _
+		.Executes(() =>
+		{
+			DotNetTest(_ => _
+				.SetProjectFile(Solution)
+				.EnableNoBuild()
+				.EnableNoRestore()
+				.SetConfiguration(Configuration));
+		});
+}
