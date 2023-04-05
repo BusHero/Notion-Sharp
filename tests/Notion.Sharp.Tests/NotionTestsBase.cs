7bc809bfe07e14564fc7a6d60aec74ad4b373501
Cervac Petru <petru.cervac@gmail.com>
2023-04-05 23:44:58 +0300

    ci: add nuke to CI (#124)
    * ci: add nuke to CI
    
    * ci: fix the issue with running nuke
    
    * ci: remove build from sln
    
    * test: fix tests
    
    * ci: fix the way nuke is invoked
    
    * ci: add cache

--- tests/Notion.Sharp.Tests/NotionTestsBase.cs
+++ tests/Notion.Sharp.Tests/NotionTestsBase.cs
@@ -4,41 +4,15 @@ namespace Notion.Sharp.Tests;
 
 public class NotionTestsBase
 {
-    internal INotion SUT { get; }
+    internal INotion Sut { get; }
 
-    internal Guid ValidUserId { get; }
-
-    internal Guid ValidDatabaseId { get; }
-
-    // internal Guid ValidPageId { get; }
-
-    internal Guid ValidBlockId { get; }
-
-    internal Guid PageFromDatabase { get; }
-
-    internal Guid SimpleDatabase { get; }
-    internal Guid ParentPage { get; }
-
-    // internal Guid DeletedPage { get; }
-    
-    // internal Guid PageWithIcon { get; }
-    
     protected NotionTestsBase()
     {
         var configuration = new ConfigurationBuilder()
             .AddUserSecrets<ModelTests>()
             .Build();
 
-        SUT = Notion.NewClient(bearerToken: configuration["Notion"], "2022-06-28");
-        // ValidUserId = Guid.Parse(configuration["userId"]);
-        ValidDatabaseId = Guid.Parse(configuration["databaseId"]);
-        // ValidPageId = Guid.Parse(configuration["pageId"]);
-        ValidBlockId = Guid.Parse(configuration["blockId"]);
-        // PageFromDatabase = Guid.Parse(configuration["pageFromDatabase"]);
-        // SimpleDatabase = Guid.Parse(configuration["simpleDatabase"]);
-        ParentPage = Guid.Parse((configuration["ParentPage"]));
-        // DeletedPage = Guid.Parse(configuration["deletedPage"]);
-        // PageWithIcon = Guid.Parse(configuration["PageWithIcon"]);
+        Sut = Notion.NewClient(bearerToken: configuration["Notion"], "2022-06-28");
     }
 
     protected static async Task RetryAsync(Func<Task> action, int attempts)
