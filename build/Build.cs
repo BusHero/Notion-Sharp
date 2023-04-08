c5d533a7b7507c156b6c4716b6b1fc2a1f6e741e
Cervac Petru <petru.cervac@gmail.com>
2023-04-08 11:26:48 +0300

    ci: publish artifacts (#126)
    * ci: publish artifacts
    
    * ci: round 2
    
    * ci: round 3
    
    * ci: disable warning
    
    * ci: make GitHub Token visible
    
    * ci: make GitHub Token invisible
    
    * ci: ensure package is tested

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,6 @@
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -13,9 +14,16 @@ class Build : NukeBuild
 
 	[Parameter] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 	
-	[Solution(GenerateProjects = true)] readonly Solution Solution;
+	[Solution(GenerateProjects = true)] readonly Solution Solution = null!;
 
-	[Parameter, Secret] readonly string NotionKey;
+	[Parameter, Secret] readonly string NotionKey = null!;
+	[Parameter, Secret] readonly string GithubToken = null!;
+
+	readonly AbsolutePath PublishFolder = RootDirectory / "publish";
+
+#pragma warning disable CA1822
+	GitHubActions GitHubActions => GitHubActions.Instance;
+#pragma warning restore CA1822
 
 	Target Restore => _ => _
 		.Executes(() =>
@@ -54,5 +62,30 @@ class Build : NukeBuild
 			DotNet($"""
 			user-secrets set "Notion" "{NotionKey}" --project {Solution.tests.Notion_Sharp_Tests}
 			""");
+		});
+	
+	Target Pack  => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			DotNetPack(_ => _
+				.SetConfiguration(Configuration)
+				.SetProject(Solution.src.Notion_Sharp)
+				.EnableIncludeSource()
+				.SetVersionSuffix(GitHubActions.RunId.ToString())
+				.SetSymbolPackageFormat("snupkg")
+				.SetOutputDirectory(PublishFolder)
+				.EnableNoRestore());
+		});
+
+	Target NugetPublish => _ => _
+		.DependsOn(Pack, Test)
+		.Executes(() =>
+		{
+			DotNetNuGetPush(_ => _
+				.SetTargetPath(PublishFolder / "*")
+				.SetSource("https://nuget.pkg.github.com/BusHero/index.json")
+				.SetApiKey(GithubToken)
+				.EnableSkipDuplicate());
 		});
 }
