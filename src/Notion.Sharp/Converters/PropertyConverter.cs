8e57ee0f453820c85fd050b75cb477ad1ee67314
Cervac Petru <petru.cervac@gmail.com>
2023-04-13 22:51:15 +0300

    feat: add support for status (#135)

--- src/Notion.Sharp/Converters/PropertyConverter.cs
+++ src/Notion.Sharp/Converters/PropertyConverter.cs
@@ -1,73 +1,114 @@
 ﻿using Notion.Model;
-
 using Pevac;
-
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
-
 using Void = Pevac.Void;
 
 namespace Notion.Converters;
 
 internal class PropertyConverter : MyJsonConverter<Property>
 {
-    public override Property Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        return Parser.ParseObject(propertyName => propertyName switch
+    public override Property Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) =>
+        Parser.ParseObject(propertyName => propertyName switch
         {
-            "id" => Parser.String.Updater((string id, Property property) => property with { Id = id }),
-            "name" => Parser.String.Updater((string name, Property property) => property with { Name = name }),
-            "type" => Parser.String.Updater<string, Property>(),
+            "id" => Parser.String.Updater((string? id, Property property) => property with { Id = id }),
+            "name" => Parser.String.Updater((string? name, Property property) => property with { Name = name }),
+            "type" => Parser.String.Updater<string?, Property>(),
             "title" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Title>()),
-            "rich_text" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.RichTextProperty>()),
-            "number" => Parser.ParseObject(propertyName => propertyName switch
+            "rich_text" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.RichTextProperty>()),
+            "number" => Parser.ParseObject(propertyName1 =>
             {
-                "format" => Parser.String.Updater((string numberFormat, Property.Number number) => number with { Format = numberFormat }),
-                var key => Parser.FailUpdate<Property.Number>($"Unknown key property.number.{key}")
+                var func = propertyName1 switch
+                {
+                    "format" => Parser.String.Updater((string? numberFormat, Property.Number number) =>
+                        number with { Format = numberFormat }),
+                    _ => Parser.FailUpdate<Property.Number>($"Unknown key property.number.{propertyName1}")
+                };
+                return func;
             }, (Property property) => property.Copy<Property.Number>()),
-            "select" => Parser.ParseObject(propertyName => propertyName switch
+            "select" => Parser.ParseObject(propertyName1 => propertyName1 switch
             {
-                "options" => Parser.ParseType<Option[]>().Updater((Option[] options, Property.Select select) => select with { Options = options }),
-                var key => Parser.FailUpdate<Property.Select>($"Unknown key property.select.{key}")
+                "options" => Parser.ParseType<Option[]>().Updater((Option[]? options1, Property.Select select) =>
+                    select with { Options = options1 }),
+                _ => Parser.FailUpdate<Property.Select>($"Unknown key property.select.{propertyName1}")
             }, (Property property) => property.Copy<Property.Select>()),
-            "multi_select" => Parser.ParseObject(propertyName => propertyName switch
+            "multi_select" => Parser.ParseObject(propertyName1 => propertyName1 switch
             {
-                "options" => Parser.ParseType<Option[]>().Updater((Option[] options, Property.MultiSelect multiSelect) => multiSelect with { Options = options }),
-                var key => Parser.FailUpdate<Property.MultiSelect>($"Unknown key property.multi_select.'{key}'")
+                "options" => Parser.ParseType<Option[]>()
+                    .Updater((Option[]? options1, Property.MultiSelect multiSelect) =>
+                        multiSelect with { Options = options1 }),
+                _ => Parser.FailUpdate<Property.MultiSelect>($"Unknown key property.multi_select.'{propertyName1}'")
             }, (Property property) => property.Copy<Property.MultiSelect>()),
             "date" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Date>()),
             "people" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.People>()),
             "files" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Files>()),
-            "checkbox" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Checkbox>()),
+            "checkbox" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.Checkbox>()),
             "url" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Url>()),
             "email" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.Email>()),
-            "phone_number" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.PhoneNumber>()),
-            "formula" => Parser.ParseObject(propertyName => propertyName switch
+            "phone_number" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.PhoneNumber>()),
+            "formula" => Parser.ParseObject(propertyName1 => propertyName1 switch
             {
-                "expression" => Parser.OptionalString.Updater((string expression, Property.Formula Formula) => Formula with { Expression = expression }),
-                var key => Parser.FailUpdate<Property.Formula>($"Unknown key property.formula.{key}")
+                "expression" => Parser.OptionalString.Updater((string? expression, Property.Formula formula) =>
+                    formula with { Expression = expression }),
+                _ => Parser.FailUpdate<Property.Formula>($"Unknown key property.formula.{propertyName1}")
             }, (Property property) => property.Copy<Property.Formula>()),
-            "relation" => Parser.ParseObject(propertyName => propertyName switch
+            "relation" => Parser.ParseObject(propertyName1 => propertyName1 switch
             {
-                "database_id" => Parser.Guid.Updater((Guid databaseId, Property.Relation relation) => relation with { DatabaseId = databaseId }),
-                "synced_property_name" => Parser.String.Updater((string syncedPropertyName, Property.Relation relation) => relation with { SyncedPropertyName = syncedPropertyName }),
-                "synced_property_id" => Parser.String.Updater((string syncedPropertyId, Property.Relation relation) => relation with { SyncedPropertyId = syncedPropertyId }),
-                var key => Parser.FailUpdate<Property.Relation>($"Unknown key property.relation.{key}")
+                "database_id" => Parser.Guid.Updater((Guid databaseId, Property.Relation relation) =>
+                    relation with { DatabaseId = databaseId }),
+                "synced_property_name" => Parser.String.Updater(
+                    (string? syncedPropertyName, Property.Relation relation) =>
+                        relation with { SyncedPropertyName = syncedPropertyName }),
+                "synced_property_id" => Parser.String.Updater(
+                    (string? syncedPropertyId, Property.Relation relation) =>
+                        relation with { SyncedPropertyId = syncedPropertyId }),
+                _ => Parser.FailUpdate<Property.Relation>($"Unknown key property.relation.{propertyName1}")
             }, (Property property) => property.Copy<Property.Relation>()),
-            "rollup" => Parser.ParseObject(propertyName => propertyName switch
+            "rollup" => Parser.ParseObject(propertyName1 => propertyName1 switch
             {
-                "rollup_property_name" => Parser.String.Updater((string rollupPropertyName, Property.Rollup rollup) => rollup with { RollupPropertyName = rollupPropertyName }),
-                "relation_property_name" => Parser.String.Updater((string relationPropertyName, Property.Rollup multiSelect) => multiSelect with { RelationPropertyName = relationPropertyName }),
-                "rollup_property_id" => Parser.String.Updater((string rollupPropertyId, Property.Rollup multiSelect) => multiSelect with { RollupPropertyId = rollupPropertyId }),
-                "relation_property_id" => Parser.String.Updater((string relationPropertyId, Property.Rollup multiSelect) => multiSelect with { RelationPropertyId = relationPropertyId }),
-                "function" => Parser.String.Updater((string function, Property.Rollup multiSelect) => multiSelect with { Function = function }),
-                var key => Parser.FailUpdate<Property.Rollup>($"Unknown key property.rollup.{key}")
+                "rollup_property_name" => Parser.String.Updater(
+                    (string? rollupPropertyName, Property.Rollup rollup) =>
+                        rollup with { RollupPropertyName = rollupPropertyName }),
+                "relation_property_name" => Parser.String.Updater(
+                    (string? relationPropertyName, Property.Rollup multiSelect) => multiSelect with
+                    {
+                        RelationPropertyName = relationPropertyName
+                    }),
+                "rollup_property_id" => Parser.String.Updater(
+                    (string? rollupPropertyId, Property.Rollup multiSelect) =>
+                        multiSelect with { RollupPropertyId = rollupPropertyId }),
+                "relation_property_id" => Parser.String.Updater(
+                    (string? relationPropertyId, Property.Rollup multiSelect) =>
+                        multiSelect with { RelationPropertyId = relationPropertyId }),
+                "function" => Parser.String.Updater((string? function, Property.Rollup multiSelect) =>
+                    multiSelect with { Function = function }),
+                _ => Parser.FailUpdate<Property.Rollup>($"Unknown key property.rollup.{propertyName1}")
             }, (Property property) => property.Copy<Property.Rollup>()),
-            "created_time" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.CreatedTime>()),
-            "created_by" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.CreatedBy>()),
-            "last_edited_time" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.LastEditedTime>()),
-            "last_edited_by" => Parser.EmptyObject.Updater((Void _, Property property) => property.Copy<Property.LastEditedBy>()),
-            var key => Parser.FailUpdate<Property>($"Unknown key property.{key}")
+            "created_time" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.CreatedTime>()),
+            "created_by" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.CreatedBy>()),
+            "last_edited_time" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.LastEditedTime>()),
+            "last_edited_by" => Parser.EmptyObject.Updater((Void _, Property property) =>
+                property.Copy<Property.LastEditedBy>()),
+            "status" => Parser.ParseObject(propertyName1 => propertyName1 switch
+            {
+                "options" => Parser.ParseType<List<Property.Status.Option>>().Updater(
+                    (List<Property.Status.Option>? options1, Property.Status status) =>
+                        status with { Options = options1 }),
+                "groups" => Parser.ParseType<List<Property.Status.Group>>().Updater(
+                    (List<Property.Status.Group>? groups, Property.Status status) =>
+                        status with { Groups = groups }),
+                _ => Parser.FailUpdate<Property.Status>($"$Unknown key property.status.{propertyName1}")
+            }, (Property property) => property.Copy<Property.Status>()),
+            _ => Parser.FailUpdate<Property>($"Unknown key property.{propertyName}")
         }).Parse(ref reader, options);
-    }
 }
--- tests/Notion.Sharp.Tests/DatabaseTests.cs
+++ tests/Notion.Sharp.Tests/DatabaseTests.cs
@@ -58,9 +58,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Text"] as Property.RichTextProperty;
-            text?.Name.Should().Be("Text");
-            text?.Id.Should().Be("%3C%3DW%3D");
+            var richTextProperty = database.Properties?["Text"] as Property.RichTextProperty;
+            richTextProperty?.Name.Should().Be("Text");
+            richTextProperty?.Id.Should().Be("%3C%3DW%3D");
         }
     }
 
@@ -75,9 +75,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Checkbox"] as Property.Checkbox;
-            text?.Name.Should().Be("Checkbox");
-            text?.Id.Should().Be("d%5BFd");
+            var checkbox = database.Properties?["Checkbox"] as Property.Checkbox;
+            checkbox?.Name.Should().Be("Checkbox");
+            checkbox?.Id.Should().Be("d%5BFd");
         }
     }
 
@@ -92,9 +92,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Created by"] as Property.CreatedBy;
-            text?.Name.Should().Be("Created by");
-            text?.Id.Should().Be("%5Ekx%7B");
+            var createdBy = database.Properties?["Created by"] as Property.CreatedBy;
+            createdBy?.Name.Should().Be("Created by");
+            createdBy?.Id.Should().Be("%5Ekx%7B");
         }
     }
 
@@ -109,9 +109,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Created time"] as Property.CreatedTime;
-            text?.Name.Should().Be("Created time");
-            text?.Id.Should().Be("LasM");
+            var createdTime = database.Properties?["Created time"] as Property.CreatedTime;
+            createdTime?.Name.Should().Be("Created time");
+            createdTime?.Id.Should().Be("LasM");
         }
     }
 
@@ -126,9 +126,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Date"] as Property.Date;
-            text?.Name.Should().Be("Date");
-            text?.Id.Should().Be("b%3C%7Cq");
+            var date = database.Properties?["Date"] as Property.Date;
+            date?.Name.Should().Be("Date");
+            date?.Id.Should().Be("b%3C%7Cq");
         }
     }
 
@@ -143,9 +143,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var text = database.Properties?["Email"] as Property.Email;
-            text?.Name.Should().Be("Email");
-            text?.Id.Should().Be("k%7CQn");
+            var email = database.Properties?["Email"] as Property.Email;
+            email?.Name.Should().Be("Email");
+            email?.Id.Should().Be("k%7CQn");
         }
     }
 
@@ -195,9 +195,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Last edited by"] as Property.LastEditedBy;
-            formula?.Name.Should().Be("Last edited by");
-            formula?.Id.Should().Be("%5DHfN");
+            var lastEditedBy = database.Properties?["Last edited by"] as Property.LastEditedBy;
+            lastEditedBy?.Name.Should().Be("Last edited by");
+            lastEditedBy?.Id.Should().Be("%5DHfN");
         }
     }
 
@@ -212,9 +212,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Last edited time"] as Property.LastEditedTime;
-            formula?.Name.Should().Be("Last edited time");
-            formula?.Id.Should().Be("%3DVc%7D");
+            var lastEditedTime = database.Properties?["Last edited time"] as Property.LastEditedTime;
+            lastEditedTime?.Name.Should().Be("Last edited time");
+            lastEditedTime?.Id.Should().Be("%3DVc%7D");
         }
     }
 
@@ -229,10 +229,10 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Number"] as Property.Number;
-            formula?.Name.Should().Be("Number");
-            formula?.Id.Should().Be("u%5Ex%3B");
-            formula?.Format.Should().Be("number");
+            var number = database.Properties?["Number"] as Property.Number;
+            number?.Name.Should().Be("Number");
+            number?.Id.Should().Be("u%5Ex%3B");
+            number?.Format.Should().Be("number");
         }
     }
 
@@ -247,9 +247,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Person"] as Property.People;
-            formula?.Name.Should().Be("Person");
-            formula?.Id.Should().Be("zK%3E_");
+            var people = database.Properties?["Person"] as Property.People;
+            people?.Name.Should().Be("Person");
+            people?.Id.Should().Be("zK%3E_");
         }
     }
 
@@ -264,9 +264,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Phone"] as Property.PhoneNumber;
-            formula?.Name.Should().Be("Phone");
-            formula?.Id.Should().Be("ODkW");
+            var phoneNumber = database.Properties?["Phone"] as Property.PhoneNumber;
+            phoneNumber?.Name.Should().Be("Phone");
+            phoneNumber?.Id.Should().Be("ODkW");
         }
     }
 
@@ -281,9 +281,9 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["URL"] as Property.Url;
-            formula?.Name.Should().Be("URL");
-            formula?.Id.Should().Be("%40iW_");
+            var url = database.Properties?["URL"] as Property.Url;
+            url?.Name.Should().Be("URL");
+            url?.Id.Should().Be("%40iW_");
         }
     }
 
@@ -298,10 +298,10 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Select"] as Property.Select;
-            formula?.Name.Should().Be("Select");
-            formula?.Id.Should().Be("u%3EXc");
-            formula?.Options.Should().BeEquivalentTo(new List<Option>
+            var select = database.Properties?["Select"] as Property.Select;
+            select?.Name.Should().Be("Select");
+            select?.Id.Should().Be("u%3EXc");
+            select?.Options.Should().BeEquivalentTo(new List<Option>
             {
                 new()
                 {
@@ -336,10 +336,10 @@ public class DatabaseTests : NotionTestsBase
         // assert
         using (new AssertionScope())
         {
-            var formula = database.Properties?["Multi-select"] as Property.MultiSelect;
-            formula?.Name.Should().Be("Multi-select");
-            formula?.Id.Should().Be("ekY%40");
-            formula?.Options.Should().BeEquivalentTo(new List<Option>
+            var multiSelect = database.Properties?["Multi-select"] as Property.MultiSelect;
+            multiSelect?.Name.Should().Be("Multi-select");
+            multiSelect?.Id.Should().Be("ekY%40");
+            multiSelect?.Options.Should().BeEquivalentTo(new List<Option>
             {
                 new()
                 {
@@ -361,6 +361,77 @@ public class DatabaseTests : NotionTestsBase
                     Name = "Option 1",
                     Color = "orange"
                 }
+            });
+        }
+    }
+    
+    [Fact]
+    public async Task Status()
+    {
+        // arrange
+
+        // act
+        var database = await Sut.GetDatabaseAsync(Databases.Status.ToGuid());
+
+        // assert
+        using (new AssertionScope())
+        {
+            var status = database.Properties?["Status"] as Property.Status;
+            status?.Name.Should().Be("Status");
+            status?.Id.Should().Be("e%3FiD");
+            status?.Options.Should().BeEquivalentTo(new List<Property.Status.Option>
+            {
+                new()
+                {
+                    Id = Guid.Parse("8e233849-de45-48cd-b191-b655ecbd46e2"),
+                    Name = "Not started",
+                    Color = "default"
+                },
+                new()
+                {
+                    Id = Guid.Parse("9d19bd7a-b686-413c-886a-f66e480c565a"),
+                    Name = "In progress",
+                    Color = "blue"
+                },
+                new()
+                {
+                    Id = Guid.Parse("09fce32d-da47-43b7-8d49-91bbed9424c0"),
+                    Name = "Done",
+                    Color = "green"
+                }
+            });
+            status?.Groups.Should().BeEquivalentTo(new List<Property.Status.Group>
+            {
+                new()
+                {
+                    Id = Guid.Parse("a45ea91c-fcd3-4b0b-b4e2-bebc5d8c02c9"),
+                    Name = "To-do",
+                    Color = "gray",
+                    OptionIds = new List<Guid>()
+                    {
+                        Guid.Parse("8e233849-de45-48cd-b191-b655ecbd46e2")
+                    }
+                },
+                new()
+                {
+                    Id = Guid.Parse("6defbcb0-cadf-423b-adbb-8583606055d9"),
+                    Name = "In progress",
+                    Color = "blue",
+                    OptionIds = new List<Guid>
+                    {
+                        Guid.Parse("9d19bd7a-b686-413c-886a-f66e480c565a")
+                    }
+                },
+                new()
+                {
+                    Id = Guid.Parse("aa70d2a9-7f9a-4c3c-9484-2839343bc298"),
+                    Name = "Complete",
+                    Color = "green",
+                    OptionIds = new List<Guid>()
+                    {
+                        Guid.Parse("09fce32d-da47-43b7-8d49-91bbed9424c0")
+                    }
+                },
             });
         }
     }
