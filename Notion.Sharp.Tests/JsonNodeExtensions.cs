b150571d93cd8e3f01ca81c8713189084b283004
bus1hero <petru.cervac@gmail.com>
2023-05-09 23:12:59 +0300

    test: add approval testsing

---
+++ Notion.Sharp.Tests/JsonNodeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Notion.Sharp.Tests;
+
+internal static class JsonNodeExtensions
+{
+    public static void IgnoreProperty(this JsonNode node, string property)
+    {
+        var words = property.Split(".");
+        var last = words.Last();
+        var temp = node;
+        foreach (var word in words.SkipLast(1))
+        {
+            temp = temp?[word];
+            if (temp is null)
+            {
+                return;
+            }
+        }
+
+        if (temp[last] is not null)
+        {
+            temp[last] = "Ignore";
+        }
+    }
+}
