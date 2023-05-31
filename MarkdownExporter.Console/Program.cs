5a3b9803a0c2140ca25b9879e61621b2ffae1f28
bus1hero <petru.cervac@gmail.com>
2023-05-31 22:02:31 +0300

    feat: add new project

---
+++ MarkdownExporter.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;using Notion;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+    
+var token = configuration["NotionClient"] ?? throw new InvalidOperationException();
+
+var client = new NotionClient(new Credentials(token));
+var page = await client.Page(Guid.Parse("ac84310baefa4282818a56211c07368e")).Get();
+foreach (var property in page!.Properties!.Keys)
+{
+    Console.WriteLine(page.Properties[property]);
+}
